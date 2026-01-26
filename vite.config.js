600e637932eb4cc36fe3e8c91cb7a4af79966770
Jinseo Yoo <155122355+yxpjseo@users.noreply.github.com>
2026-01-26 21:06:52 +0900

    ✨ GitHub 템플릿 및 Vercel 배포 워크플로우 추가

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,7 @@ import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    eslint({
-      failOnError: false,
-    }),
-  ],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
