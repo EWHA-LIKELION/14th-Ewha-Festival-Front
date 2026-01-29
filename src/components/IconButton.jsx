de70be198ed4e7ed4399d1896d898da91761c51e
Min-hye-Kim <kimilu@ewha.ac.kr>
2026-01-29 23:02:10 +0900

    💄feat : icon Button 공통컴포넌트(#5)

---
+++ src/components/IconButton.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const ICONS = {
+  bookmark: '/icons/icon-bookmark.svg',
+  chevronleft: '/icons/icon-chevronleft.svg',
+  pencil: '/icons/icon-pencil.svg',
+  search: '/icons/icon-search.svg',
+  trash: '/icons/icon-trash.svg',
+  greenarrow: '/icons/icon-greenarrow.svg',
+  xmarkcircle: '/icons/icon-xmarkcircle.svg',
+  xmarkwhite: '/icons/icon-xmarkwhite.svg',
+  github: '/icons/logo-github.svg',
+  instagram: '/icons/logo-instagram.svg',
+  instagramcolor: '/icons/logo-instagramcolor.svg',
+  kakaotalk: '/icons/logo-kakaotalk.svg',
+  likelion: '/icons/logo-likelion.svg',
+};
+
+function IconButton({ name, size = '24', alt = '', className = '', onClick }) {
+  return (
+    <button type="button" onClick={onClick} className={className}>
+      <img src={ICONS[name]} alt={alt || name} width={size} height={size} />
+    </button>
+  );
+}
+
+export default IconButton;
