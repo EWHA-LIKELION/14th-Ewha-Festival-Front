11949a6fd7e69ba6dc58a60952446602a55531bd
ye-xins <yerins.cse@ewha.ac.kr>
2026-02-05 01:02:27 +0900

    ✨ feat: toast 공통 컴포넌트 퍼블리싱

--- src/components/ComponentPreview.jsx
+++ src/components/ComponentPreview.jsx
@@ -4,6 +4,7 @@
 
 import React from 'react';
 import Alert from '@/components/Alert';
+import Toast from '@/components/Toast';
 
 const ComponentPreview = () => {
   return (
@@ -20,6 +21,17 @@ const ComponentPreview = () => {
           }
         />
         <Alert variant="login" title="공지" text={<>언제든지 다시 로그인하실 수 있어요.</>} />
+        <Toast text="리스트가 등록되었어요." />
+
+        <div className="relative h-50">
+          {/* 배경 */}
+          <img src="/images/test.jpg" className="absolute inset-0 h-full w-full object-cover" />
+
+          {/* Toast */}
+          <div className="relative z-10 flex justify-center pt-10">
+            <Toast text="리스트가 등록되었어요." />
+          </div>
+        </div>
       </div>
     </>
   );
