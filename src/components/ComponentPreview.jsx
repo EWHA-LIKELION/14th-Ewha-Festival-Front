8e13a5012587d5b7b2f9761f25d025a88264b8f6
Jinseo Yoo <155122355+yxpjseo@users.noreply.github.com>
2026-02-04 22:02:19 +0900

    ✨Footer 컴포넌트 퍼블리싱

--- src/components/ComponentPreview.jsx
+++ src/components/ComponentPreview.jsx
@@ -3,11 +3,13 @@
  */
 
 import React from 'react';
+import Footer from '@/components/Footer';
 
 const ComponentPreview = () => {
   return (
     <>
       <div>ComponentPreview</div>
+      <Footer />
     </>
   );
 };
