141226ff5a12a85eef229b54197c4d1750232d1d
Jinseo Yoo <155122355+yxpjseo@users.noreply.github.com>
2026-02-05 19:37:28 +0900

    ✨ 네비게이션 바 퍼블리싱

---
+++ src/components/NavigationBar.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import { useNavigate, useMatch } from 'react-router-dom';
+
+const NavigationBar = () => {
+  const navigate = useNavigate();
+
+  // Hooks를 최상위에서 호출하여 순서 보장
+  const matchHome = useMatch({ path: '/', end: true });
+  const matchMap = useMatch('/map/*');
+  const matchMy = useMatch('/my/*');
+
+  const navItems = [
+    {
+      iconActive: '/public/icons/icon-home-selected.svg',
+      iconInactive: '/public/icons/icon-home-unselected.svg',
+      label: '홈',
+      path: '/',
+      isActive: matchHome,
+    },
+    {
+      iconActive: '/public/icons/icon-location-selected.svg',
+      iconInactive: '/public/icons/icon-location-unselected.svg',
+      label: '부스/공연',
+      path: '/map',
+      isActive: matchMap,
+    },
+    {
+      iconActive: '/public/icons/icon-my-selected.svg',
+      iconInactive: '/public/icons/icon-my-unselected.svg',
+      label: '마이',
+      path: '/my',
+      isActive: matchMy,
+    },
+  ];
+
+  return (
+    <div
+      className="fixed bottom-0 left-1/2 z-50 flex h-20 w-[392px] -translate-x-1/2 justify-around border-t border-gray-100 bg-white pt-3 text-xs font-medium"
+      style={{ paddingBottom: 'env(safe-area-inset-bottom)' }}
+    >
+      {navItems.map((item) => {
+        return (
+          <button
+            key={item.label}
+            onClick={() => navigate(item.path)}
+            className={`flex h-fit w-15 flex-col items-center justify-center gap-1 ${
+              item.isActive ? 'text-emerald-500' : 'text-gray-500'
+            }`}
+          >
+            <img src={item.isActive ? item.iconActive : item.iconInactive} alt={item.label} />
+            <p>{item.label}</p>
+          </button>
+        );
+      })}
+    </div>
+  );
+};
+
+export default NavigationBar;
