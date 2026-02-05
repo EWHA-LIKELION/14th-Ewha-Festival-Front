9bebfd53c783f0102ad5041a6fb0fa4b2c402e7f
ye-xins <yerins.cse@ewha.ac.kr>
2026-02-05 17:56:00 +0900

    ♻️  fix: 이전 pr과 충돌해결

--- src/components/ComponentPreview.jsx
+++ src/components/ComponentPreview.jsx
@@ -22,16 +22,6 @@ const ComponentPreview = () => {
         />
         <Alert variant="login" title="공지" text={<>언제든지 다시 로그인하실 수 있어요.</>} />
         <Toast text="리스트가 등록되었어요." />
-
-        <div className="relative h-50">
-          {/* 배경 */}
-          <img src="/images/test.jpg" className="absolute inset-0 h-full w-full object-cover" />
-
-          {/* Toast */}
-          <div className="relative z-10 flex justify-center pt-10">
-            <Toast text="리스트가 등록되었어요." />
-          </div>
-        </div>
       </div>
     </>
   );
