f83ce466a499eb41f4793f74cb54386672fb4958
Min-hye-Kim <kimilu@ewha.ac.kr>
2026-02-10 23:18:26 +0900

    ✨feat : Timepicker 컴포넌트 퍼블리싱(#35)

--- src/components/ComponentPreview.jsx
+++ src/components/ComponentPreview.jsx
@@ -4,26 +4,16 @@
 
 import React from 'react';
 import Footer from '@/components/Footer';
-import Radio from '@/components/Radio';
+import Timepicker from '@/components/Timepicker';
 
 const ComponentPreview = () => {
   return (
     <>
       <div>ComponentPreview</div>
       <div className="p-4">
-      <br/>
-      <Radio label="라디오 버튼" selected={false} error={false} showLabel={true} onChange={() => {}}/>
-      <br/>
-      <Radio label="라디오 버튼" selected={true} error={false} showLabel={true} onChange={() => {}}/>
-      <br/>
-      <Radio label="라디오 버튼" selected={false} error={true} showLabel={true} onChange={() => {}}/>
-      <br/>
-      <Radio label="" selected={false} error={false} showLabel={false} onChange={() => {}}/>
-      <br/>
-      <Radio label="" selected={true} error={false} showLabel={false} onChange={() => {}}/>
-      <br/>
-      <Radio label="" selected={false} error={true} showLabel={false} onChange={() => {}}/>
-        </div>
+        <Timepicker isActive={true} />
+        <Timepicker isActive={false} />
+      </div>
     </>
   );
 };
