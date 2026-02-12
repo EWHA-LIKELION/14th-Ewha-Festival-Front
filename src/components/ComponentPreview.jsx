c60f32fdeac384d7e7eaba905a537f66b24f1737
Minhye Kim <kimilu@ewha.ac.kr>
2026-02-12 10:30:57 +0900

    🐛fix : ComponentPreview 에서 탭 전환 안 되던 문제 해결(#39)
    ComponentPreview 에서 탭 전환 안 되던 문제 해결

--- src/components/ComponentPreview.jsx
+++ src/components/ComponentPreview.jsx
@@ -2,16 +2,20 @@
  * 공통 컴포넌트 프리뷰 페이지
  */
 
-import React from 'react';
+import React, { useState } from 'react';
 import Tab from '@/components/Tab';
 
 const ComponentPreview = () => {
+  const [activeIndex1, setActiveIndex1] = useState(0);
+  const [activeIndex2, setActiveIndex2] = useState(0);
+  const [activeIndex3, setActiveIndex3] = useState(0);
+  const [activeIndex4, setActiveIndex4] = useState(0);
   return (
     <>
       <div className="p-8">
         <div>Type 1 탭</div>
         <br />
-        <Tab tabs={['부스', '공연']} activeIndex={0} onChange={(i) => setActiveIndex(i)} />
+        <Tab tabs={['부스', '공연']} activeIndex={activeIndex1} onChange={setActiveIndex1} />
         <br />
         <br />
         <br />
@@ -20,15 +24,15 @@ const ComponentPreview = () => {
         <Tab
           variant="underline"
           tabs={['리스트', '후기']}
-          activeIndex={0}
-          onChange={(i) => setActiveIndex(i)}
+          activeIndex={activeIndex3}
+          onChange={setActiveIndex3}
         />
         <br />
         <Tab
           variant="underline"
           tabs={['세트리스트', '후기']}
-          activeIndex={1}
-          onChange={(i) => setActiveIndex(i)}
+          activeIndex={activeIndex4}
+          onChange={setActiveIndex4}
         />
       </div>
     </>
