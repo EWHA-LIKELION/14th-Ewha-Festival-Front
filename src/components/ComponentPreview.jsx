915228719ff037a9c649232559f540b45ae31a66
Jinseo Yoo <155122355+yxpjseo@users.noreply.github.com>
2026-02-12 21:15:18 +0900

    ✨ 캐러셀 프리뷰 추가

--- src/components/ComponentPreview.jsx
+++ src/components/ComponentPreview.jsx
@@ -3,13 +3,38 @@
  */
 
 import React from 'react';
-import DropDown from '@/components/DropDown';
+import Carousel from '@/components/Carousel';
 
 const ComponentPreview = () => {
   return (
     <>
       <div className="flex h-100 items-center justify-center">
-        <DropDown />
+        <Carousel
+          items={[
+            {
+              image: 'images/boothcard-test.jpg',
+              title: '첫 번째 제목',
+              description: '첫 번째 설명',
+              link: '/',
+            },
+            {
+              image: 'images/carousel-test1.png',
+              title: '두 번째 제목',
+              description: '두 번째 설명',
+              link: 'https://likelion.ewha.university/',
+            },
+            {
+              image: 'images/carousel-test2.png',
+              title: '세 번째 제목',
+              description: '세 번째 설명',
+            },
+            {
+              image: 'images/carousel-test1.png',
+              title: '네 번째 제목',
+              description: '네 번째 설명',
+            },
+          ]}
+        />
       </div>
     </>
   );
