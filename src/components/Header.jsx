7529c6db3dd3d2431e2a15a77b5eaffa94389b23
Jinseo Yoo <155122355+yxpjseo@users.noreply.github.com>
2026-02-13 18:50:38 +0900

    ✨ Header 컴포넌트의 로고 크기를 6에서 10으로 변경

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -79,9 +79,9 @@ const Header = ({
         )}
         {left === 'logo' && (
           <img
-            src="/icons/icon-logo.svg"
+            src="/icons/icon-logo.svg" // 추후 업데이트 예정
             alt="logo"
-            className="h-6"
+            className="h-10" // 추후 수정 예정
             onClick={() => navigate('/')}
           />
         )}
