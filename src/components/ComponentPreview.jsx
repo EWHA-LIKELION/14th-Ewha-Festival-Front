5b9e8373dc4772297f8a264b6aa8ce09eaf86113
Jinseo Yoo <155122355+yxpjseo@users.noreply.github.com>
2026-02-17 23:00:25 +0900

    ✨ 버튼 컴포넌트 프리뷰 설정

--- src/components/ComponentPreview.jsx
+++ src/components/ComponentPreview.jsx
@@ -2,40 +2,134 @@
  * 공통 컴포넌트 프리뷰 페이지
  */
 
-import React, { useState } from 'react';
-import Tab from '@/components/Tab';
+import React from 'react';
+import Button from '@/components/Button';
+import SearchBar from '@/components/SearchBar';
 
 const ComponentPreview = () => {
-  const [activeIndex1, setActiveIndex1] = useState(0);
-  const [activeIndex2, setActiveIndex2] = useState(0);
-  const [activeIndex3, setActiveIndex3] = useState(0);
-  const [activeIndex4, setActiveIndex4] = useState(0);
   return (
-    <>
-      <div className="p-8">
-        <div>Type 1 탭</div>
-        <br />
-        <Tab tabs={['부스', '공연']} activeIndex={activeIndex1} onChange={setActiveIndex1} />
-        <br />
-        <br />
-        <br />
-        <div>Type 2 탭</div>
-        <br />
-        <Tab
-          variant="underline"
-          tabs={['리스트', '후기']}
-          activeIndex={activeIndex3}
-          onChange={setActiveIndex3}
-        />
-        <br />
-        <Tab
-          variant="underline"
-          tabs={['세트리스트', '후기']}
-          activeIndex={activeIndex4}
-          onChange={setActiveIndex4}
-        />
-      </div>
-    </>
+    <div className="flex flex-col items-center bg-amber-200">
+      <br />
+      <SearchBar />
+      <br />
+      <SearchBar isMap />
+      <br />
+      <h1>[IconOnlyButton]</h1>
+      <br />
+      <Button leftIcon="/icons/icon-search.svg"></Button>
+      <br />
+      <Button variant="bg-gray" size="md" circle leftIcon="/icons/icon-search.svg"></Button>
+      <br />
+      <Button variant="text-gray" size="sm" circle leftIcon="/icons/icon-search.svg"></Button>
+      <br />
+      <br />
+      <h1>[IconOnlyButton Disabled]</h1>
+      <br />
+      <Button disabled leftIcon="/icons/icon-search.svg"></Button>
+      <br />
+      <Button
+        disabled
+        variant="bg-gray"
+        size="md"
+        circle
+        leftIcon="/icons/icon-search.svg"
+      ></Button>
+      <br />
+      <Button
+        disabled
+        variant="text-black"
+        size="sm"
+        circle
+        leftIcon="/icons/icon-search.svg"
+      ></Button>
+      <br />
+      <br />
+      <h1>[Button]</h1>
+      <br />
+      <Button leftIcon="/icons/icon-search.svg" rightIcon="/icons/icon-search.svg">
+        Button
+      </Button>
+      <br />
+      <Button
+        variant="bg-white"
+        shadow
+        circle
+        leftIcon="/icons/icon-search.svg"
+        rightIcon="/icons/icon-search.svg"
+      >
+        Button
+      </Button>
+      <br />
+      <Button variant="underline-green">Button</Button>
+      <br />
+      <Button
+        variant="bg-red"
+        size="md"
+        leftIcon="/icons/icon-search.svg"
+        rightIcon="/icons/icon-search.svg"
+      >
+        Button
+      </Button>
+      <br />
+      <Button
+        variant="bg-gray"
+        size="md"
+        circle
+        leftIcon="/icons/icon-search.svg"
+        rightIcon="/icons/icon-search.svg"
+      >
+        Button
+      </Button>
+      <br />
+      <Button variant="underline-gray" size="md">
+        Button
+      </Button>
+      <br />
+      <Button
+        variant="bg-gray"
+        size="sm"
+        leftIcon="/icons/icon-search.svg"
+        rightIcon="/icons/icon-search.svg"
+      >
+        Button
+      </Button>
+      <br />
+      <Button
+        variant="bg-pink"
+        size="sm"
+        circle
+        leftIcon="/icons/icon-search.svg"
+        rightIcon="/icons/icon-search.svg"
+      >
+        Button
+      </Button>
+      <br />
+      <Button variant="underline-white" size="sm">
+        Button
+      </Button>
+      <br />
+      <br />
+      <h1>[Button Disabled]</h1>
+      <br />
+      <Button disabled leftIcon="/icons/icon-search.svg" rightIcon="/icons/icon-search.svg">
+        Button
+      </Button>
+      <br />
+      <Button
+        variant="text-black"
+        disabled
+        size="md"
+        leftIcon="/icons/icon-search.svg"
+        rightIcon="/icons/icon-search.svg"
+      >
+        Button
+      </Button>
+      <br />
+      <Button variant="underline-green" disabled size="sm">
+        Button
+      </Button>
+      <br />
+    </div>
   );
 };
 
