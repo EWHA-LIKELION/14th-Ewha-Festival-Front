e41bb4c5e460c8b67638746ff0c4dbb61d04ffba
ye-xins <yerins.cse@ewha.ac.kr>
2026-03-10 21:23:09 +0900

    main pull 전 임시 저장

--- src/pages/admin/AdminConfirmPage.jsx
+++ src/pages/admin/AdminConfirmPage.jsx
@@ -1,3 +1,16 @@
 /**
  * 관리자 코드 인증 페이지
  */
+
+import React from 'react';
+import Header from '@/components/Header';
+
+const AdminConfirmPage = () => {
+  return (
+    <>
+      <></>
+    </>
+  );
+};
+
+export default AdminConfirmPage;
