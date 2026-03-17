b00e5ec1785c75b5f4f0e940f6d593965a8a9709
Jinseo Yoo <155122355+yxpjseo@users.noreply.github.com>
2026-03-18 02:21:25 +0900

    ✨ Footer, Carousel 프리뷰 추가

--- src/components/ComponentPreview.jsx
+++ src/components/ComponentPreview.jsx
@@ -2,51 +2,33 @@
  * 공통 컴포넌트 프리뷰 페이지
  */
 
-import React, { useState } from 'react';
-import Header from '@/components/Header';
-import BottomsheetDrag from '@/components/BottomsheetDrag';
-import useBottomsheetStore from '@/store/useBottomsheetStore';
-import BoothCard from '@/components/Card/BoothCard';
+import Carousel from '@/components/Carousel';
+import Footer from '@/components/Footer';
 
 const ComponentPreview = () => {
-  const sheetSize = useBottomsheetStore((s) => s.sheetSize);
-
   return (
     <>
-      <BottomsheetDrag>
-        {sheetSize === 'full' && (
-          <>
-            <Header left="back" background="gradient" />
-            <img
-              src="/images/boothcard-test.jpg"
-              alt=""
-              className="w-full"
-              style={{ height: '240px', objectFit: 'cover', marginTop: '-4.5rem' }}
-            />
-          </>
-        )}
-        <div className="h-200 p-5">
-          {/* BoothCard 프리뷰 */}
-          <div className="mb-8">
-            <h3 className="mb-3 text-lg font-semibold">BoothCard</h3>
-            <BoothCard
-              name="라이크라이언 떡볶이"
-              category="음식"
-              days="3/1~3/3"
-              location="학생회관 앞"
-              description="매콤달콤한 떡볶이와 다양한 간식이 준비되어 있습니다."
-              thumbnail="/images/boothcard-test.jpg"
-              images={[
-                '/images/boothcard-test.jpg',
-                '/images/carousel-test1.png',
-                '/images/carousel-test2.png',
-              ]}
-              status="open"
-              onClick={() => {}}
-            />
-          </div>
-        </div>
-      </BottomsheetDrag>
+      <div className="flex h-100 items-center justify-center">
+        <Carousel
+          items={[
+            {
+              image: 'images/boothcard-test.jpg',
+              link: '/',
+            },
+            {
+              image: 'images/carousel-test1.png',
+              link: 'https://likelion.ewha.university/',
+            },
+            {
+              image: 'images/carousel-test2.png',
+            },
+            {
+              image: 'images/carousel-test1.png',
+            },
+          ]}
+        />
+      </div>
+      <Footer />
     </>
   );
 };
