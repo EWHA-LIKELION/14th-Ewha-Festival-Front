0ece5ab6482356cc3bb6e183df8a590cc4f75804
Jinseo Yoo <155122355+yxpjseo@users.noreply.github.com>
2026-03-20 01:42:57 +0900

    ♻️ App.jsx: 컴포넌트 및 라우트 정리

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,34 @@
 import { Routes, Route } from 'react-router-dom';
+
+// 레이아웃
 import NavigationBarLayout from '@/layouts/NavigationBarLayout';
-import { ToastManager } from '@/components/Toast';
-import useToastStore from '@/store/useToastStore';
-import ComponentPreview from '@/components/ComponentPreview';
+
+// 전역 상태 관리
 import LoginSheet from '@/features/LoginSheet';
 import useAuthStore from '@/store/useAuthStore';
-import MyPage from '@/pages/my/MyPage';
-import MyBoothPage from '@/pages/admin/MyBoothPage';
-import AdminConfirmPage from '@/pages/admin/AdminConfirmPage';
-import MyShowPage from '@/pages/admin/MyShowPage';
+import { ToastManager } from '@/components/Toast';
+import useToastStore from '@/store/useToastStore';
+
+// 홈 & 기타 페이지
 import SearchPage from '@/pages/SearchPage';
-import BarrierFreeSheet from '@/features/BarrierFreeSheet';
-import TrashSheet from '@/features/TrashSheet';
+import NoticePage from '@/pages/NoticePage';
+
+// 마이
+import MyPage from '@/pages/my/MyPage';
+
+// 지도
 import MapPage from '@/pages/MapPage';
+import BoothListSheet from '@/features/booth/BoothListSheet';
+import TrashSheet from '@/features/TrashSheet';
+import BarrierFreeSheet from '@/features/BarrierFreeSheet';
+
+// 컴포넌트 프리뷰
+import ComponentPreview from '@/components/ComponentPreview';
+
+// 관리자
+import AdminConfirmPage from '@/pages/admin/AdminConfirmPage';
+import MyBoothPage from '@/pages/admin/MyBoothPage';
+import MyShowPage from '@/pages/admin/MyShowPage';
 
 function App() {
   const showLoginSheet = useAuthStore((s) => s.showLoginSheet);
@@ -24,6 +40,9 @@ function App() {
         <Route element={<NavigationBarLayout />}>
           {/* 홈 */}
           <Route path="/" element={<div className="h-300 bg-zinc-500">Home</div>} />
+          <Route path="credit" element={<div>Credit</div>} />
+          <Route path="notice" element={<div>Notice</div>} />
+          <Route path="search" element={<SearchPage />} />
 
           {/* 마이 */}
           <Route path="my" element={<MyPage />} />
@@ -31,35 +50,33 @@ function App() {
 
           {/* 지도 */}
           <Route path="map" element={<MapPage />}>
-            <Route path="booths" element={<div>BoothListSheet</div>} />
+            <Route path="booths" element={<BoothListSheet />} />
             <Route path="booths/:id" element={<div>BoothDetailSheet</div>} />
+            <Route path="booths/:id/notice" element={<NoticePage />} />
             <Route path="shows" element={<div>ShowListSheet</div>} />
             <Route path="shows/:id" element={<div>ShowDetailSheet</div>} />
+            <Route path="shows/:id/notice" element={<NoticePage />} />
             <Route path="trash" element={<TrashSheet />} />
             <Route path="barrierfree" element={<BarrierFreeSheet />} />
           </Route>
 
-          {/* 공통 컴포넌트 퍼블리싱 기간이 끝나면 아래 라우트는 삭제 */}
+          {/* 🔥 추후 공통컴포넌트 라우트는 삭제 */}
           <Route path="component-preview" element={<ComponentPreview />} />
         </Route>
 
-        <Route path="credit" element={<div>Credit</div>} />
-        <Route path="introduction" element={<div>Introduction</div>} />
-        <Route path="notice" element={<div>Notice</div>} />
-
-        <Route path="search" element={<SearchPage />} />
         <Route path="admin">
-          <Route path="confirm" element={<div>Confirm</div>} />
-          <Route path="booth/:id" element={<MyBoothPage />} />
           <Route path="confirm" element={<AdminConfirmPage />} />
-          <Route path="booth/:id" element={<div>My Booth</div>} />
+          <Route path="booth/:id" element={<MyBoothPage />} />
           <Route path="booth/:id/edit" element={<div>Booth Edit</div>} />
+          <Route path="booth/:id/notice" element={<NoticePage />} />
           <Route path="show/:id" element={<MyShowPage />} />
           <Route path="show/:id/edit" element={<div>Show Edit</div>} />
+          <Route path="show/:id/notice" element={<NoticePage />} />
         </Route>
       </Routes>
 
-      {/* 로그인 바텀시트 — 전역에서 openLoginSheet()으로 호출 */}
+      {/* ⚙️ 전역 상태 관리 */}
+      {/* 로그인 바텀시트 */}
       {showLoginSheet && <LoginSheet />}
       {/* 토스트 */}
       <ToastManager text={text} isOpen={isOpen} onClose={closeToast} />
