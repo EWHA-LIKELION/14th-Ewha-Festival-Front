fcdeb7622cfe5c34ca5c87f74383a6dc1628c5be
Jinseo Yoo <155122355+yxpjseo@users.noreply.github.com>
2026-04-01 23:44:41 +0900

    ✨ 필터 구성 및 카테고리, 정렬, 위치, 날짜 상수 추가

---
+++ src/constants/building.js
@@ -0,0 +1,19 @@
+export const BOOTH_LOCATION = [
+  { value: 'HUMAN_ECOLOGY_BUILDING', label: '생활환경관' },
+  { value: 'STUDENT_UNION', label: '학생문화관' },
+  { value: 'EWHA_POSCO_BUILDING', label: '포스코관' },
+  { value: 'HAK_GWAN', label: '학관' },
+  { value: 'GRASS_GROUND', label: '잔디광장' },
+
+  { value: 'WELCH_RYANG_AUDITORIUM', label: '대강당' },
+  { value: 'HYUUT_GIL', label: '휴웃길' },
+  { value: 'EDUCATION_BUILDING', label: '교육관' },
+  { value: 'EWHA_SHINSEGAE_BUILDING', label: '신세계관' },
+  { value: 'SPORT_TRACK', label: '스포츠트랙' },
+];
+
+export const SHOW_LOCATION = [
+  { value: 'STUDENT_UNION', label: '학생문화관' },
+  { value: 'SPORT_TRACK', label: '스포츠트랙' },
+  { value: 'GRASS_GROUND', label: '잔디광장' },
+];
---
+++ src/constants/sort.js
@@ -0,0 +1,11 @@
+export const BOOTH_SORT = [
+  { value: 'number', label: '번호순', isDefault: true },
+  { value: 'scrap', label: '스크랩순' },
+  { value: 'name', label: '이름순' },
+];
+
+export const SHOW_SORT = [
+  { value: 'time', label: '시간순', isDefault: true },
+  { value: 'scrap', label: '스크랩순' },
+  { value: 'name', label: '이름순' },
+];
