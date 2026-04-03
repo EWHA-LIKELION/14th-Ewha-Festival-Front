291511791031bd74462f43947508445b7077f49e
Jinseo Yoo <155122355+yxpjseo@users.noreply.github.com>
2026-04-03 18:45:22 +0900

    ✨ 전역 로딩 상태 관리 store 추가

---
+++ src/store/useLoadingStore.js
@@ -0,0 +1,15 @@
+/**
+ * Loading 전역 상태 관리 store (Zustand)
+ */
+
+import { create } from 'zustand';
+
+const useLoadingStore = create((set) => ({
+  isLoading: false,
+
+  showLoading: () => set({ isLoading: true }),
+
+  hideLoading: () => set({ isLoading: false }),
+}));
+
+export default useLoadingStore;
