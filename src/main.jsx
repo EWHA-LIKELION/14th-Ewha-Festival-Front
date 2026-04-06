29a2d0e0d1be672dead597e57d5aaf8d23d776af
Jinseo Yoo <155122355+yxpjseo@users.noreply.github.com>
2026-04-06 22:43:44 +0900

    ✨  TanstackQuery 설정

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,28 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import './index.css';
 import App from './App.jsx';
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 1, // api 요청 실패 시 재시도 횟수
+      refetchOnWindowFocus: false, // 브라우저 탭 전환 후 돌아올 때 자동으로 데이터 새로고침 X
+      staleTime: 1000 * 60 * 5, // 5분
+    },
+  },
+});
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </StrictMode>,
 );
