e68d6c941c69c26dfcacd6ccacb3d2ad794bb8f1
Jinseo Yoo <155122355+yxpjseo@users.noreply.github.com>
2026-04-08 18:43:45 +0900

    ✨ useInfiniteScroll 훅 추가: 무한 스크롤 기능 구현

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -4,3 +4,4 @@
 export { default as useImageUploader } from './useImageUploader.js';
 export { useSearch } from './useSearch.js';
 export { useBooths } from './useBooths.js';
+export { useInfiniteScroll } from './useInfiniteScroll.js';
