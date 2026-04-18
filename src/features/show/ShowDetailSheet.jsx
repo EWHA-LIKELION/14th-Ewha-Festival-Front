02221a77b862939eaacc16098d80916fb5e4de5f
Jinseo Yoo <155122355+yxpjseo@users.noreply.github.com>
2026-04-19 02:17:24 +0900

    🎨 ShowDetailSheet: 코드 포맷 적용

--- src/features/show/ShowDetailSheet.jsx
+++ src/features/show/ShowDetailSheet.jsx
@@ -103,174 +103,181 @@ const ShowDetailSheet = () => {
           </>
         )}
 
-        {!isLoading && <div className="flex w-full flex-col items-center gap-9 pt-5">
-          {/* 부스 설명 */}
-          <div className="flex w-full flex-col gap-6 self-stretch px-5">
-            <div className="flex w-full flex-col items-start gap-2 self-stretch">
-              <div className="flex w-full items-center justify-between self-stretch">
-                <h2 className="text-2xl leading-8 font-semibold tracking-normal text-zinc-800">
-                  {show.name || '공연명'}
-                </h2>
-                <ScrapButton id={id} type="show" initialScrapped={show.is_scrapped} count={show.scraps_count} />
-              </div>
-
-              {(categoryText || show.is_ongoing !== undefined) && (
-                <div className="flex items-center gap-1">
-                  {categoryText && (
-                    <p className="text-sm leading-5 font-normal tracking-normal text-zinc-500">
-                      {categoryText}
-                    </p>
-                  )}
-
-                  {show.is_ongoing !== undefined && (
-                    <>
-                      {categoryText && <img src="/icons/icon-eclipse-gray.svg" />}
-                      <Badge state={getShowState(show.is_ongoing)} size="md" />
-                    </>
-                  )}
+        {!isLoading && (
+          <div className="flex w-full flex-col items-center gap-9 pt-5">
+            {/* 부스 설명 */}
+            <div className="flex w-full flex-col gap-6 self-stretch px-5">
+              <div className="flex w-full flex-col items-start gap-2 self-stretch">
+                <div className="flex w-full items-center justify-between self-stretch">
+                  <h2 className="text-2xl leading-8 font-semibold tracking-normal text-zinc-800">
+                    {show.name || '공연명'}
+                  </h2>
+                  <ScrapButton
+                    id={id}
+                    type="show"
+                    initialScrapped={show.is_scrapped}
+                    count={show.scraps_count}
+                  />
                 </div>
-              )}
 
-              {show.description && (
-                <p className="line-clamp-2 max-h-10 self-stretch text-sm leading-5 font-normal tracking-normal text-zinc-500">
-                  {show.description}
-                </p>
-              )}
-            </div>
+                {(categoryText || show.is_ongoing !== undefined) && (
+                  <div className="flex items-center gap-1">
+                    {categoryText && (
+                      <p className="text-sm leading-5 font-normal tracking-normal text-zinc-500">
+                        {categoryText}
+                      </p>
+                    )}
 
-            <Divider />
-
-            <div className="flex-start flex flex-col gap-4 self-stretch">
-              {/* 시간 */}
-              <div className="flex items-start gap-4">
-                <h3 className="w-7 text-center text-sm leading-5 font-semibold tracking-normal text-zinc-500">
-                  시간
-                </h3>
-                <div className="flex flex-col items-start gap-0.5">
-                  {scheduleText.length > 0 ? (
-                    scheduleText.map((t, i) => (
-                      <h3
-                        key={i}
-                        className="text-sm leading-5 font-normal tracking-normal text-zinc-800"
-                      >
-                        {t}
-                      </h3>
-                    ))
-                  ) : (
-                    <h3 className="text-sm leading-5 font-normal tracking-normal text-zinc-800">
-                      -
-                    </h3>
-                  )}
-                </div>
-              </div>
-
-              {/* 위치 */}
-              <div className="flex items-start gap-4">
-                <h3 className="w-7 text-center text-sm leading-5 font-semibold tracking-normal text-zinc-500">
-                  위치
-                </h3>
-                <div className="flex flex-col items-start gap-1.5">
-                  <div className="flex items-center gap-1.5">
-                    <button
-                      className={`text-sm leading-5 font-medium tracking-normal text-zinc-800 ${
-                        locationName
-                          ? 'underline decoration-solid underline-offset-2'
-                          : 'cursor-default'
-                      }`}
-                    >
-                      {locationName || '-'}
-                    </button>
-
-                    {show.roadview && (
+                    {show.is_ongoing !== undefined && (
                       <>
-                        <img src="/icons/icon-eclipse-gray.svg" />
-                        <button
-                          className="text-sm leading-5 font-medium tracking-normal text-zinc-800 underline decoration-solid underline-offset-2"
-                          onClick={() => {
-                            setSelectedImage(show.roadview);
-                            setShowModal(true);
-                          }}
-                        >
-                          로드뷰
-                        </button>
+                        {categoryText && <img src="/icons/icon-eclipse-gray.svg" />}
+                        <Badge state={getShowState(show.is_ongoing)} size="md" />
                       </>
                     )}
                   </div>
+                )}
 
-                  {show.location_description && (
-                    <p className="text-xs leading-4 font-normal tracking-normal text-zinc-500">
-                      {show.location_description}
-                    </p>
-                  )}
-                </div>
+                {show.description && (
+                  <p className="line-clamp-2 max-h-10 self-stretch text-sm leading-5 font-normal tracking-normal text-zinc-500">
+                    {show.description}
+                  </p>
+                )}
               </div>
 
-              {/* SNS */}
-              <div className="flex items-start gap-4">
-                <h3 className="w-7 text-center text-sm leading-5 font-semibold tracking-normal text-zinc-500">
-                  SNS
-                </h3>
-                <div className="flex items-center gap-2.5">
-                  {snsLinks.instagram && (
-                    <img
-                      src="/icons/logo-instagramcolor.svg"
-                      className="h-7 w-7 cursor-pointer rounded-md"
-                      onClick={() => window.open(snsLinks.instagram, '_blank')}
-                    />
-                  )}
+              <Divider />
 
-                  {snsLinks.kakaotalk && (
-                    <img
-                      src="/icons/logo-kakaotalkcolor.svg"
-                      className="h-7 w-7 cursor-pointer rounded-md"
-                      onClick={() => window.open(snsLinks.kakaotalk, '_blank')}
-                    />
-                  )}
+              <div className="flex-start flex flex-col gap-4 self-stretch">
+                {/* 시간 */}
+                <div className="flex items-start gap-4">
+                  <h3 className="w-7 text-center text-sm leading-5 font-semibold tracking-normal text-zinc-500">
+                    시간
+                  </h3>
+                  <div className="flex flex-col items-start gap-0.5">
+                    {scheduleText.length > 0 ? (
+                      scheduleText.map((t, i) => (
+                        <h3
+                          key={i}
+                          className="text-sm leading-5 font-normal tracking-normal text-zinc-800"
+                        >
+                          {t}
+                        </h3>
+                      ))
+                    ) : (
+                      <h3 className="text-sm leading-5 font-normal tracking-normal text-zinc-800">
+                        -
+                      </h3>
+                    )}
+                  </div>
+                </div>
 
-                  {!snsLinks.instagram && !snsLinks.kakaotalk && (
-                    <p className="text-sm leading-5 font-normal text-zinc-500">-</p>
-                  )}
+                {/* 위치 */}
+                <div className="flex items-start gap-4">
+                  <h3 className="w-7 text-center text-sm leading-5 font-semibold tracking-normal text-zinc-500">
+                    위치
+                  </h3>
+                  <div className="flex flex-col items-start gap-1.5">
+                    <div className="flex items-center gap-1.5">
+                      <button
+                        className={`text-sm leading-5 font-medium tracking-normal text-zinc-800 ${
+                          locationName
+                            ? 'underline decoration-solid underline-offset-2'
+                            : 'cursor-default'
+                        }`}
+                      >
+                        {locationName || '-'}
+                      </button>
+
+                      {show.roadview && (
+                        <>
+                          <img src="/icons/icon-eclipse-gray.svg" />
+                          <button
+                            className="text-sm leading-5 font-medium tracking-normal text-zinc-800 underline decoration-solid underline-offset-2"
+                            onClick={() => {
+                              setSelectedImage(show.roadview);
+                              setShowModal(true);
+                            }}
+                          >
+                            로드뷰
+                          </button>
+                        </>
+                      )}
+                    </div>
+
+                    {show.location_description && (
+                      <p className="text-xs leading-4 font-normal tracking-normal text-zinc-500">
+                        {show.location_description}
+                      </p>
+                    )}
+                  </div>
+                </div>
+
+                {/* SNS */}
+                <div className="flex items-start gap-4">
+                  <h3 className="w-7 text-center text-sm leading-5 font-semibold tracking-normal text-zinc-500">
+                    SNS
+                  </h3>
+                  <div className="flex items-center gap-2.5">
+                    {snsLinks.instagram && (
+                      <img
+                        src="/icons/logo-instagramcolor.svg"
+                        className="h-7 w-7 cursor-pointer rounded-md"
+                        onClick={() => window.open(snsLinks.instagram, '_blank')}
+                      />
+                    )}
+
+                    {snsLinks.kakaotalk && (
+                      <img
+                        src="/icons/logo-kakaotalkcolor.svg"
+                        className="h-7 w-7 cursor-pointer rounded-md"
+                        onClick={() => window.open(snsLinks.kakaotalk, '_blank')}
+                      />
+                    )}
+
+                    {!snsLinks.instagram && !snsLinks.kakaotalk && (
+                      <p className="text-sm leading-5 font-normal text-zinc-500">-</p>
+                    )}
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
 
-          {/* 공지 */}
-          <div className="w-full px-5">
-            {show.latest_notice ? (
-              <NoticeCard
-                title={show.latest_notice.title}
-                onClick={goNoticePage}
-                style={{ cursor: 'pointer' }}
-              />
-            ) : (
-              <NoticeCard title="등록된 공지가 없어요." />
-            )}
-          </div>
-
-          {/* 리스트 */}
-          <div className="flex w-full flex-col items-center gap-2 self-stretch px-5">
-            <Tab
-              variant="underline"
-              tabs={['세트리스트']}
-              activeIndex={activeTab}
-              onChange={(index) => setActiveTab(index)}
-            />
-            <div className="flex w-full flex-col items-start self-stretch">
-              {activeTab === 0 && (
-                <div className="flex w-full flex-col gap-2 pb-36">
-                  {show.setlist && show.setlist.length > 0 ? (
-                    show.setlist.map((item) => <SetlistCard key={item.id} title={item.name} />)
-                  ) : (
-                    <div className="flex w-full items-center justify-center self-stretch py-20 text-center text-base leading-6 font-normal tracking-normal text-zinc-300">
-                      등록된 내용이 없어요.
-                    </div>
-                  )}
-                </div>
+            {/* 공지 */}
+            <div className="w-full px-5">
+              {show.latest_notice ? (
+                <NoticeCard
+                  title={show.latest_notice.title}
+                  onClick={goNoticePage}
+                  style={{ cursor: 'pointer' }}
+                />
+              ) : (
+                <NoticeCard title="등록된 공지가 없어요." />
               )}
             </div>
+
+            {/* 리스트 */}
+            <div className="flex w-full flex-col items-center gap-2 self-stretch px-5">
+              <Tab
+                variant="underline"
+                tabs={['세트리스트']}
+                activeIndex={activeTab}
+                onChange={(index) => setActiveTab(index)}
+              />
+              <div className="flex w-full flex-col items-start self-stretch">
+                {activeTab === 0 && (
+                  <div className="flex w-full flex-col gap-2 pb-36">
+                    {show.setlist && show.setlist.length > 0 ? (
+                      show.setlist.map((item) => <SetlistCard key={item.id} title={item.name} />)
+                    ) : (
+                      <div className="flex w-full items-center justify-center self-stretch py-20 text-center text-base leading-6 font-normal tracking-normal text-zinc-300">
+                        등록된 내용이 없어요.
+                      </div>
+                    )}
+                  </div>
+                )}
+              </div>
+            </div>
           </div>
-        </div>}
+        )}
 
         {showModal && <ImageModal image={selectedImage} onClose={() => setShowModal(false)} />}
       </BottomsheetDrag>
