705d245569243bc89f5a4b835502ba7210c45dd7
Jinseo Yoo <155122355+yxpjseo@users.noreply.github.com>
2026-05-01 23:20:02 +0900

    💄 베리어프리 바텀시트 내용 수정 및 텍스트 색상 수정

--- src/features/BarrierFreeSheet.jsx
+++ src/features/BarrierFreeSheet.jsx
@@ -22,7 +22,7 @@ const BarrierFreeSheet = () => {
       </div>
       <BottomsheetDrag>
         {isFull && <Header left="back" center="title" centerTitle="배리어프리" isSheet />}
-        <div className="flex flex-col gap-10 p-5">
+        <div className="flex flex-col gap-10 p-5 text-zinc-800">
           <div className="flex flex-col gap-1">
             <h1 className="text-base font-semibold">이용 대상</h1>
             <p className="py-1 text-sm font-normal">
@@ -34,9 +34,11 @@ const BarrierFreeSheet = () => {
             <h1 className="text-base font-semibold">사전 신청 안내</h1>
             <p className="py-1 text-sm font-normal">
               ･ 동반 1인까지 신청 가능
-              <br />･ 동반인 포함 선착순 20명
+              <br />･ 선착순 6명(동반인 포함 12명)
               <br />
-              <br />･ 사전 신청 기간: 5월 7일(수) 12:00 - 5월 8일(금) 18:00
+              <br />･ 사전 신청 기간:
+              <br />
+              &nbsp;&nbsp;5월 15일(금) 20:00 - 5월 16일(토) 23:59
               <br />
               <br />･ 사전 신청 과정에서 배리어프리존 악용 사례를 방지하고 원활한 운영을 위해, 실물
               장애복지카드 촬영본, 병원 진단서(원본 혹은 스캔본) 등의 자료 수합
@@ -45,7 +47,7 @@ const BarrierFreeSheet = () => {
           <div className="flex flex-col gap-1">
             <h1 className="text-base font-semibold">제공 서비스</h1>
             <p className="py-1 text-sm font-normal">
-              ･ 속기 2명(구글 독스 형식으로 실시간 속기 제공)
+              ･ 속기(구글 독스 형식으로 실시간 속기 제공)
               <br />･ 테이블 및 의자
               <br />･ 배리어프리존 전담 스태프
             </p>
