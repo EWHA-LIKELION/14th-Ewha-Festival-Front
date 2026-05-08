c8241c9925ea0557911c260220a08a092a007ba3
Jinseo Yoo <155122355+yxpjseo@users.noreply.github.com>
2026-05-09 02:03:16 +0900

    ✨ Scrim 컴포넌트에서 useEffect를 사용하여 body overflow 상태 관리 추가

--- src/components/Scrim.jsx
+++ src/components/Scrim.jsx
@@ -2,9 +2,18 @@
  * Scrim 컴포넌트
  */
 
-import React from 'react';
+import { useEffect } from 'react';
 
 const Scrim = ({ onClick }) => {
+  useEffect(() => {
+    const prevBodyOverflow = document.body.style.overflow;
+    document.body.style.overflow = 'hidden';
+
+    return () => {
+      document.body.style.overflow = prevBodyOverflow;
+    };
+  }, []);
+
   return <div onClick={onClick} className="fixed inset-0 z-40 bg-black/50"></div>;
 };
 
