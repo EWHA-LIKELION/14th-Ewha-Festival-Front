2f0b91b1ad8ed8dee97aeca8dcb43c657d806a7e
Jinseo Yoo <155122355+yxpjseo@users.noreply.github.com>
2026-05-08 20:32:15 +0900

    ♻️ 부스 및 공연 관련 훅 추가 및 기존 훅 통합

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -2,15 +2,29 @@
 //사용하는 쪽에서 import { useIsMobile } from "@/hooks"; 이렇게 불러와서 사용
 
 export { default as useImageUploader } from './useImageUploader.js';
-export { useSearch } from './useSearch.js';
-export { useBooths } from './useBooths.js';
-export { useShows } from './useShows.js';
-export { useInfiniteScroll } from './useInfiniteScroll.js';
 export { useInfiniteList, buildQueryParams } from './useInfiniteList.js';
-export { usePopularKeywords } from './usePopularKeywords.js';
-export { useSearchResults } from './useSearchResults.js';
-export { default as useMapFocus } from './map/useMapFocus.js';
-export { default as useMapAssets } from './map/useMapAssets.js';
-export { default as useMapActiveSync } from './map/useMapActiveSync.js';
-export { default as useMapFilterSync } from './map/useMapFilterSync.js';
+export { useInfiniteScroll } from './useInfiniteScroll.js';
 export { default as useScrollToTop } from './useScrollToTop.js';
+
+// booth
+export { useBooths } from './booth/useBooths.js';
+export { useBoothDetail, useBoothNotices, useUpdateBooth } from './booth/useBoothDetail.js';
+
+// show
+export { useShows } from './show/useShows.js';
+export { useShowDetail, useShowNotices, useUpdateShow } from './show/useShowDetail.js';
+
+// my
+export { useMyProfile } from './my/useMyProfile.js';
+export { useMyScrapBooths, useMyScrapShows } from './my/useMyScrap.js';
+
+// search
+export { usePopularKeywords } from './search/usePopularKeywords.js';
+export { useSearch } from './search/useSearch.js';
+export { useSearchResults } from './search/useSearchResults.js';
+
+// map
+export { default as useMapActiveSync } from './map/useMapActiveSync.js';
+export { default as useMapAssets } from './map/useMapAssets.js';
+export { default as useMapFilterSync } from './map/useMapFilterSync.js';
+export { default as useMapFocus } from './map/useMapFocus.js';
