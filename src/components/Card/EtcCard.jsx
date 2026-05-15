f86f3b7ef47d77a7daa8d064efd6676ffeca0a79
yxpjse0 <7wry4mrn2m@privaterelay.appleid.com>
2026-05-15 22:04:23 +0900

    💄 기타시설 카드 description truncate 설정

--- src/components/Card/EtcCard.jsx
+++ src/components/Card/EtcCard.jsx
@@ -6,14 +6,14 @@ const EtcCard = ({ title, description, selected = false, onClick }) => {
   return (
     <div
       onClick={onClick}
-      className={`flex w-full cursor-pointer flex-col items-start justify-center rounded-xl border p-5 ${selected ? 'border-emerald-400 bg-emerald-50' : 'border-zinc-200 bg-white'}`}
+      className={`flex w-full cursor-pointer flex-col items-start justify-center overflow-hidden rounded-xl border p-5 ${selected ? 'border-emerald-400 bg-emerald-50' : 'border-zinc-200 bg-white'}`}
     >
-      <div className="flex h-11.5 w-78 flex-col items-start gap-1.5">
-        <h2 className="self-stretch text-base leading-6 font-semibold tracking-normal text-zinc-800">
+      <div className="flex h-11.5 w-full min-w-0 flex-col items-start gap-1.5">
+        <h2 className="w-full text-base leading-6 font-semibold tracking-normal text-zinc-800">
           {title}
         </h2>
         <p
-          className={`self-stretch text-xs leading-4 font-normal tracking-normal ${selected ? 'text-zinc-800' : 'text-zinc-500'}`}
+          className={`w-full truncate text-xs leading-4 font-normal tracking-normal ${selected ? 'text-zinc-800' : 'text-zinc-500'}`}
         >
           {description}
         </p>
